960b6446e5e295566b83473fd1c5411383fbb121
Jochem Bakers <Jbakers@users.noreply.github.com>
2025-11-16 05:19:19 +0100

    Version: 3.5.0-dev.10 - Uninstall: Add remote MOTD removal option + fix borders

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Smart Release Tool - Combines version bumping with intelligent CHANGELOG generation
 # Author: J.Bakers
-# Version: 3.5.0-dev.9
+# Version: 3.5.0-dev.10
 
 # Colors
 CYAN='\033[0;36m'
